47be023f7c25de9e514e940fc017ef307389db24
luishs0 <luishs.doc@gmail.com>
2022-11-08 14:32:59 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
 
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
 </script>
 
 
 <template>
 
-  <div class="container">
-
-
-
-  </div>
+  <header>
+    <AppHeader />
+  </header>
 
 </template>
 
 
 <style lang="scss" scoped>
+@use "./styles/general.scss" as *;
 
+header {
+  max-width: 1200px;
+  margin: auto;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -63,9 +63,68 @@ export default {
 
 <template>
 
+    <div class="container-header">
+
+        <div class="header-left">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+
+
+        <div class="header-right">
+            <ul>
+                <li v-for="(link, index) in links"><a href=""> {{ link.title }} </a></li>
+            </ul>
+        </div>
+
+    </div>
+
 </template>
 
 
 <style lang="scss" scoped>
+.container-header {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
 
+    max-width: 1200px;
+
+
+
+    width: 80%;
+    margin: auto;
+
+    .header-left {
+        width: 20%;
+        text-align: center;
+
+        img {
+            padding: .2rem;
+            height: 5rem;
+        }
+    }
+
+    .header-right {
+        width: 75%;
+
+        ul {
+            display: flex;
+            flex-wrap: wrap;
+            gap: .8rem;
+        }
+
+        li {
+            text-decoration: none;
+            list-style-type: none;
+            font-size: .8rem;
+
+            a {
+                text-decoration: none;
+                color: black;
+            }
+        }
+    }
+}
 </style>
